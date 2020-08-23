01b6f1ffffadc20d29d1ea684bf87fdc15500f85
Yaroslav <yaroslav.olex@gmail.com>
2020-08-23 16:57:24 +0300

    Final project

--- PickYourColor/ViewController.swift
+++ PickYourColor/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redIntensityLabel: UILabel!
+    @IBOutlet var greenIntensityLabel: UILabel!
+    @IBOutlet var blueIntensityLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        redIntensityLabel.text = String(redSlider.value)
+        greenIntensityLabel.text = String(greenSlider.value)
+        blueIntensityLabel.text = String(blueSlider.value)
+        
+        showColor()
     }
 
 
+    @IBAction func redSliderAction() {
+        redIntensityLabel.text = String((redSlider.value * 100).rounded() / 100)
+        showColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenIntensityLabel.text = String((greenSlider.value * 100).rounded() / 100)
+        showColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueIntensityLabel.text = String((blueSlider.value * 100).rounded() / 100)
+        showColor()
+    }
+    
+    
+    private func showColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
 }
 
